f73de006bb26d58f1cbb1bbb79ec43d156491231
shaishab316 <shaishabchandrashil@gmail.com>
2025-10-28 16:21:06 +0600

    feat: Enhance security with rate limiting and update dependencies
    This commit introduces rate limiting middleware for OTP verification,
    login, forgot password, and registration endpoints to prevent abuse.
    These limiters restrict the number of requests within a defined time
    window. It also updates `@typescript-eslint` dependencies to version
    7.18.0, adjusts node engine support, adds `peer: true` to
    `@octokit/plugin-throttling` and `express-validator`, and enables
    optional TypeScript peer dependency.

--- src/app/modules/auth/Auth.route.ts
+++ src/app/modules/auth/Auth.route.ts
@@ -5,35 +5,46 @@ import { UserControllers } from '../user/User.controller';
 import { UserValidations } from '../user/User.validation';
 import purifyRequest from '../../middlewares/purifyRequest';
 import auth from '../../middlewares/auth';
+import {
+  otpVerifyRateLimiter,
+  forgotPasswordRateLimiter,
+  loginRateLimiter,
+  registerRateLimiter,
+} from './Auth.utils';
 
 const free = Router();
 {
   free.post(
     '/register',
+    registerRateLimiter,
     purifyRequest(UserValidations.userRegister),
     UserControllers.register,
   );
 
   free.post(
     '/agent-register',
+    registerRateLimiter,
     purifyRequest(UserValidations.agentRegister),
     UserControllers.register,
   );
 
   free.post(
     '/venue-register',
+    registerRateLimiter,
     purifyRequest(UserValidations.venueRegister),
     UserControllers.register,
   );
 
   free.post(
     '/artist-register',
+    registerRateLimiter,
     purifyRequest(UserValidations.artistRegister),
     UserControllers.register,
   );
 
   free.post(
     '/organizer-register',
+    registerRateLimiter,
     purifyRequest(UserValidations.organizerRegister),
     UserControllers.register,
   );
@@ -41,12 +52,14 @@ const free = Router();
 
 free.post(
   '/account-verify',
+  otpVerifyRateLimiter,
   purifyRequest(AuthValidations.accountVerify),
   AuthControllers.accountVerify,
 );
 
 free.post(
   '/login',
+  loginRateLimiter,
   purifyRequest(AuthValidations.login),
   AuthControllers.login,
 );
@@ -59,12 +72,14 @@ free.post(
 
 free.post(
   '/forgot-password',
+  forgotPasswordRateLimiter,
   purifyRequest(AuthValidations.otpSend),
   AuthControllers.forgotPassword,
 );
 
 free.post(
   '/forgot-password/otp-verify',
+  otpVerifyRateLimiter,
   purifyRequest(AuthValidations.accountVerify),
   AuthControllers.forgotPasswordOtpVerify,
 );
