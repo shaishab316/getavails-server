ece6c1cbb41cc498f235d803cf962ad38aaa8b67
shaishab316 <shaishabchandrashil@gmail.com>
2025-11-05 17:58:11 +0600

    feat: Schedule event publishing and add CLI spinner
    Adds cron job to publish upcoming events and integrates ora for a CLI spinner.
    Includes job cleanup on server exit. Removes ora from devDependencies and
    adds it to dependencies.

---
+++ src/app/modules/event/Event.job.ts
@@ -0,0 +1,46 @@
+/* eslint-disable no-console */
+import cron from 'node-cron';
+import { EEventStatus, prisma } from '../../../utils/db';
+import ora from 'ora';
+import chalk from 'chalk';
+
+/**
+ * Event jobs
+ *
+ * @returns unsubscribe function
+ */
+export function eventJobs() {
+  console.log(chalk.green('Event jobs started'));
+
+  //? every day at 7am
+  const publishingJob = cron.schedule('0 7 * * *', async () => {
+    const spinner = ora(chalk.yellow('Publishing events...')).start();
+
+    try {
+      await prisma.event.updateMany({
+        where: {
+          status: EEventStatus.UPCOMING,
+          start_date: {
+            lte: new Date(),
+          },
+        },
+        data: {
+          status: EEventStatus.PUBLISHED,
+          published_at: new Date(),
+        },
+      });
+
+      spinner.succeed(chalk.green('Events published successfully'));
+    } catch (error) {
+      if (error instanceof Error) {
+        spinner.fail(chalk.red(`Error publishing events: ${error.message}`));
+      }
+    }
+  });
+
+  //? return unsubscribe function
+  return () => {
+    publishingJob.destroy();
+    console.log(chalk.green('Event jobs stopped'));
+  };
+}
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import startServer from './utils/server/startServer';
 import { SocketServices } from './app/modules/socket/Socket.service';
+import { eventJobs } from './app/modules/event/Event.job';
 
 startServer().then(server => {
   //? add server plugins
@@ -7,10 +8,14 @@ startServer().then(server => {
   //? socket plugins
   const socketCleanup = SocketServices.init(server);
 
+  //? event jobs
+  const eventJobsCleanup = eventJobs();
+
   //? cleanup on process close
   ['SIGINT', 'SIGTERM'].forEach(signal =>
     process.once(signal, async () => {
       socketCleanup();
+      eventJobsCleanup();
 
       server.close(() => process.exit(0));
     }),
